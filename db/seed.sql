15f8c3e3d497390864c023bcf0a8d641060657fc
Chito <Javier.martinez632@gmail.com>
2022-12-08 14:14:12 -0700

    updated seed.sql/ server.js

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,22 @@
 INSERT INTO department(id, name)
-VALUES (,,,);
+VALUES (1, 'HR'),
+       (2, 'sales'),
+       (3, 'Finance'),
+       (4, 'legal'),
+       (5, 'Engineering')
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES ();
+VALUES (1, 'HR Representative', 90000, 1),
+       (2, 'HR Team Lead', 110000, 1), 
+       (3, 'Sales Representative', 60000, 2),
+       (4, 'Sales Team Lead', 75000, 2),
+       (5, 'Accountant', 125000, 3),
+       (6, 'Account Manager', 160000, 3),
+       (7, 'Lawyer', 160000, 4),
+       (8, 'Legal Team Lead', 250000, 4)
+       (9, 'Software Engineer', 120000, 5),
+       (10, 'Lead Engineer', 160000, 5); 
 
 INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
-VALUES ();
+VALUES (1, 'David', 'Vasquez', 'HR Representative', 'null'),
+       (2, 'Sam', 'Smith', 'HR Team Lead', ) 
--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const express = require('express');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-inquirer
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'Maeve_Benjen25',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+  );
+
+
+  inquirer
   .prompt([
     {
       type: 'input',
       message: 'What would you like to do?',
       name: 'options',
-      choices: ['View All Employees', 'Add Employee', 'Update Empoyee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+      choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'Quit']
     },
-    
-  ]);
+  ])
+// a query database for displaying in command-line
+db.query('SELECT * FROM company_db', function (err, results){
+    console.log(results);
+});
+// default response
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port`)
+});
