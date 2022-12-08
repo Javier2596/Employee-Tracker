71c4ae1d9ac4b964a58333359c478e3fc3d8be68
Chito <Javier.martinez632@gmail.com>
2022-12-07 18:42:31 -0700

    updated schema.sql, seed.sql

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS company_db;
+
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee ( 
+    id: INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT 
+);
--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What would you like to do?',
+      name: 'options',
+      choices: ['View All Employees', 'Add Employee', 'Update Empoyee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+    },
+    
+  ]);
